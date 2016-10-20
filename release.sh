ee0327a588ce3a49aeb09f98ac5fe294b99f4de8
dyu <david.yu.ftw@gmail.com>
2016-10-20 14:45:08 +0800

    mvn env var

--- release.sh
+++ release.sh
@@ -11,9 +11,11 @@ PR_VERSION=$2
 
 echo "Releasing $VERSION - are you sure? (y/n):" && read CONFIRM && [ "$CONFIRM" != "y" ] && exit 0
 
-mvn versions:set -DnewVersion=$VERSION && \
+[ ! -n "$MVN"] && MVN=mvn
+
+$MVN versions:set -DnewVersion=$VERSION && \
 git clean -f && git add -u && git commit -m "$VERSION" && \
-mvn -Prelease deploy && mvn scm:tag && \
-mvn versions:set -DnewVersion=$PR_VERSION && \
+$MVN -Prelease deploy && $MVN scm:tag && \
+$MVN versions:set -DnewVersion=$PR_VERSION && \
 git clean -f && git add -u && git commit -m "$PR_VERSION" && \
 git push origin master
